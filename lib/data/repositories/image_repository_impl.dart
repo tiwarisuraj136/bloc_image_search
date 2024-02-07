9887d71d82e1948a94d13304103823c18eb5eb67
LENOVO <paarthbhansali@gmail.com>
2024-02-07 12:16:48 +0530

    image search but i want some changes in application like that background image , appbar etc 7.2.24 time 12.16pm

---
+++ lib/data/repositories/image_repository_impl.dart
@@ -0,0 +1,14 @@
+import 'package:bloc_image_search/data/datasources/remote/image_datasource.dart';
+import 'package:bloc_image_search/domain/entities/image_entity.dart';
+import 'package:bloc_image_search/domain/repositories/image_repository.dart';
+
+class ImageRepositoryImpl implements ImageRepository {
+  final ImageDataSource dataSource;
+
+  ImageRepositoryImpl({required this.dataSource});
+
+  @override
+  Future<List<ImageEntity>> fetchImages(String apiKey, String query) {
+    return dataSource.getSearchedImages(apiKey, query);
+  }
+}
